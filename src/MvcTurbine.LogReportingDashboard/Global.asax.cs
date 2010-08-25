ea572805d12a4cd90b0cbe949b4f897a49e0f8d0
darrencauthon <darren@cauthon.com>
2010-08-25 13:05:49 -0500

    Made some progress.

--- src/MvcTurbine.LogReportingDashboard/Global.asax.cs
+++ src/MvcTurbine.LogReportingDashboard/Global.asax.cs
@@ -1,34 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
+using MvcTurbine.ComponentModel;
+using MvcTurbine.Routing;
+using MvcTurbine.Unity;
+using MvcTurbine.Web;
 
 namespace MvcTurbine.LogReportingDashboard
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : TurbineApplication
     {
-        public static void RegisterRoutes(RouteCollection routes)
+        public MvcApplication()
+        {
+            ServiceLocatorManager.SetLocatorProvider(() => new UnityServiceLocator());
+        }
+    }
+
+    public class DefaultRouting : IRouteRegistrator
+    {
+        public void Register(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-
-        }
-
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-
-            RegisterRoutes(RouteTable.Routes);
+                new {controller = "Home", action = "Index", id = UrlParameter.Optional} // Parameter defaults
+                );
         }
     }
 }
