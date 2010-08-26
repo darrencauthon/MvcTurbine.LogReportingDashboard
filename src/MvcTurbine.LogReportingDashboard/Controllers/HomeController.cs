a42d8cbeb2568269129c7963a73f7301836d5f22
darrencauthon <darren@cauthon.com>
2010-08-25 21:22:45 -0500

    Commit latest play.

--- src/MvcTurbine.LogReportingDashboard/Controllers/HomeController.cs
+++ src/MvcTurbine.LogReportingDashboard/Controllers/HomeController.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcLoggingDemo.Services.Logging;
 
 namespace MvcTurbine.LogReportingDashboard.Controllers
 {
     [HandleError]
     public class HomeController : Controller
     {
+        private readonly ILogger logger;
+
+        public HomeController(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
         public ActionResult Index()
         {
             ViewData["Message"] = "Welcome to ASP.NET MVC!";
 
+            logger.Debug("TESTING");
+            logger.Info("INFO");
+            logger.Fatal("FATAL");
+            logger.Warn("FATAL");
             return View();
         }
 
