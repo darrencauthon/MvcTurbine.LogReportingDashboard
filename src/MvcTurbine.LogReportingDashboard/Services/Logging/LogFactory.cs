3cb3d134eb2aad462b77f0966c2e8fe0ec24ec5d
darrencauthon <darren@cauthon.com>
2010-08-28 08:53:56 -0500

    More progress.

---
+++ src/MvcTurbine.LogReportingDashboard/Services/Logging/LogFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+
+namespace MvcTurbine.LogReportingDashboard.Services.Logging
+{
+    public static class LogFactory
+    {
+        /// <summary>
+        /// This static class returns the logger that is configured through the appSettings section of the web.config file
+        /// The default logger is NLog
+        /// </summary>
+        /// <returns>A concrete logger, usually an NLogLogger or Log4NetLogger</returns>
+        public static ILogger Logger()
+        {
+            string defaultLoggerTypeName = "MvcTurbine.LogReportingDashboard.Services.Logging.NLog.NLogLogger";
+            string loggerTypeName = ConfigurationManager.AppSettings["loggerTypeName"];
+            loggerTypeName = (loggerTypeName == null) ? defaultLoggerTypeName : loggerTypeName;
+
+            Type loggerType = Type.GetType(loggerTypeName);
+            ILogger logger = Activator.CreateInstance(loggerType) as ILogger;
+
+            return logger;
+        }
+    }
+}
