5d77520fdb746bcbee2a1708990f519cdb76b05d
darrencauthon <darren@cauthon.com>
2010-08-29 16:03:19 -0500

    Add nlog setup blade to set utc time correctly, cleanup.

---
+++ src/MvcTurbine.LogReportingDashboard/Blades/NLogSetupBlade.cs
@@ -0,0 +1,15 @@
+﻿using MvcTurbine.Blades;
+using MvcTurbine.LogReportingDashboard.Services.Logging.NLog;
+using NLog;
+
+namespace MvcTurbine.LogReportingDashboard.Blades
+{
+    public class NLogSetupBlade : Blade
+    {
+        public override void Spin(IRotorContext context)
+        {
+            LayoutRendererFactory.AddLayoutRenderer("utc_date", typeof (UtcDateRenderer));
+            LayoutRendererFactory.AddLayoutRenderer("web_variables", typeof (WebVariablesRenderer));
+        }
+    }
+}
