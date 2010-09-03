01497120fb059128f4240d3fb2734fe81daae0f7
darrencauthon <darren@cauthon.com>
2010-09-02 21:53:24 -0500

    Add route constraint on sample to prevent logging from appearing to work when routing has not been defined.

--- src/SampleApplication/Routing/DefaultRegistration.cs
+++ src/SampleApplication/Routing/DefaultRegistration.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Routing;
 using MvcTurbine.Routing;
 
@@ -13,8 +16,27 @@ namespace SampleApplication.Routing
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new {controller = "Home", action = "Index", id = UrlParameter.Optional} // Parameter defaults
+                new {controller = "Home", action = "Index", id = UrlParameter.Optional}, // Parameter defaults
+                new {controller = new ControllerInThisAssemblyRestraint()}
                 );
+        }
+    }
+
+    public class ControllerInThisAssemblyRestraint : IRouteConstraint
+    {
+        private readonly IEnumerable<string> list;
+
+        public ControllerInThisAssemblyRestraint()
+        {
+            list = (this).GetType().Assembly
+                .GetTypes()
+                .Where(x => x.BaseType == typeof (Controller))
+                .Select(x => x.Name.Substring(0, x.Name.Length - "Controller".Length));
+        }
+
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            return list.Contains((string) values["controller"]);
         }
     }
 }
