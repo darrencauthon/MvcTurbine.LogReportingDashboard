2aebbe01663b3d2e0573ef885ebabce456edaf29
darrencauthon <darren@cauthon.com>
2010-09-02 21:11:46 -0500

    Create logging configuration.

---
+++ src/MvcTurbine.LogReportingDashboard/LoggingConfiguration.cs
@@ -0,0 +1,35 @@
+﻿namespace MvcTurbine.LogReportingDashboard
+{
+    public class LoggingConfiguration : IConfigureLogging
+    {
+        private string pageName = "Logging";
+        private string key;
+        private string value;
+        private bool requireTheUserToBeAuthenticated;
+
+        public void Configure()
+        {
+            RequireTheUserToBeAuthenticated();
+        }
+
+        protected void RequireTheUserToBeAuthenticated()
+        {
+            requireTheUserToBeAuthenticated = true;
+        }
+
+        protected void LookForThisValueInTheQueryString(string value)
+        {
+            this.value = value;
+        }
+
+        protected void LookForThisKeyInTheQueryString(string key)
+        {
+            this.key = key;
+        }
+
+        protected void UseThisPage(string pageName)
+        {
+            this.pageName = pageName;
+        }
+    }
+}
