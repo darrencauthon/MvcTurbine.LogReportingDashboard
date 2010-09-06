1a67a10511ce09f95d8f28e3d250040ec8793376
Darren Cauthon <darren@cauthon.com>
2010-09-06 15:49:55 -0500

    Set first version to 0.7.

--- src/MvcTurbine.LogReportingDashboard/Properties/AssemblyInfo.cs
+++ src/MvcTurbine.LogReportingDashboard/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DEG")]
 [assembly: AssemblyProduct("MvcTurbine.LogReportingDashboard")]
-[assembly: AssemblyCopyright("Copyright © DEG 2010")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
