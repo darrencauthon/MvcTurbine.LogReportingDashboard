e1125f7a9ac3f398034dd3b504b5ce6543783730
Darren Cauthon <darren@cauthon.com>
2010-09-06 15:36:28 -0500

    Add file from previous commit.

---
+++ src/SampleApplication/Logging/DefaultLoggingConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using MvcTurbine.LogReportingDashboard;
+using MvcTurbine.LogReportingDashboard.Logging;
+using MvcTurbine.LogReportingDashboard.Models.Repository;
+
+namespace SampleApplication.Logging
+{
+    public class DefaultLoggingConfiguration : LoggingConfiguration
+    {
+        public DefaultLoggingConfiguration(LoggingRouteData loggingRouteData, LoggerExclusionSet loggerExclusionSet)
+            : base(loggingRouteData, loggerExclusionSet)
+        {
+        }
+
+        public override void Configure()
+        {
+            UseThisPage("Logging1234");
+            //ExcludeThisLogger(typeof(HealthMonitoringRepository));
+            //ExcludeThisLogger(typeof(ElmahRepository));
+        }
+    }
+}
